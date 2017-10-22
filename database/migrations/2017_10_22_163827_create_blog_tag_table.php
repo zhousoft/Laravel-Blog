a49f122abd86c8b0e6dd85c613be4d2c50941f9b
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-10-23 00:57:02 +0800

    增加tag表

---
+++ database/migrations/2017_10_22_163827_create_blog_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_tag', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name', 20);
+            $table->integer('count');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_tag');
+    }
+}
