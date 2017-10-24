715bfe9cc9e6219fc48a528f0461e84853de4405
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-10-24 22:45:47 +0800

    调整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -198,11 +198,7 @@
     <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.bootcss.com/popper.js/1.12.5/popper.min.js"></script>
     <script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    {{--  <script src="vendor/jquery/jquery.min.js"></script>  --}}
-    {{--  <script src="vendor/popper/popper.min.js"></script>  --}}
-    {{--  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>  --}}
-
+    
   </body>
 
 </html>
