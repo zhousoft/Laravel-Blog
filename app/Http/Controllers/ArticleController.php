cfb6bc88db4eaf42853684e24441aeed6723bcfc
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-11-03 22:12:15 +0800

    增加文章控制器

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Post;
 class ArticleController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $article = Post::find($id);
+        return view('front.article', compact('$article'));
+    }
 }
