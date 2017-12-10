db0de1b61525c4b5902fd46fa663b5c9d1170dae
zhouzhongtao <zhouzhongtaosoft@163.com>
2017-12-10 22:20:55 +0800

    修复地址

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -8,8 +8,8 @@
     @include('admin.sidebar')
 @endsection
 
-@section('content-header', "系统信息")
-@section('content-description', "当前系统状态")
+@section('content-header', "分类管理")
+@section('content-description', "文章分类管理")
     
 @section('content')
   分类管理
